605e990b308d2c8f5f26fcbf3121aca2564a8673
khuncodeadsw <khuncodeasdw1379@gmail.com>
2022-08-22 13:23:34 +0700

    add secret key and psycopg2-binary

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['PROPAGATE_EXCEPTIONS'] = True
